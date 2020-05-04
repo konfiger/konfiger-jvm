b8300749b2f1603cc4d1a5c6df372800b28598b6
Azeez Adewale <azeezadewale98@gmail.com>
2020-05-04 16:29:07 +0100

    re-write KonfigerUtil to use the new escape char and ignore other escape sequence

--- src/main/java/io/github/thecarisma/KonfigerUtil.java
+++ src/main/java/io/github/thecarisma/KonfigerUtil.java
@@ -6,54 +6,15 @@ class KonfigerUtil {
         StringBuilder finalValue = new StringBuilder();
         for (int i = 0; i < value.length(); ++i) {
             char c = value.charAt(i);
-            switch (c) {
-                case '\t':
-                    finalValue.append("\\t");
-                    break;
-                case '\n':
-                    finalValue.append("\\n");
-                    break;
-                case '\r':
-                    finalValue.append("\\r");
-                    break;
-                case '\f':
-                    finalValue.append("\\f");
-                    break;
-                case '\b':
-                    finalValue.append("\\b");
-                    break;
-                case '\\':
-                    if (extraEscapes.length > 0) {
-                        for (char extraEscape : extraEscapes) {
-                            if (value.charAt(i) == extraEscape) {
-                                finalValue.append(value.charAt(i));
-                                break;
-                            }
-                        }
-                    } else {
-                        finalValue.append("\\\\");
+            if (extraEscapes.length > 0) {
+                for (char extra : extraEscapes) {
+                    if (c == extra) {
+                        finalValue.append('/');
+                        break;
                     }
-                    break;
-                case '\'':
-                    finalValue.append("\\'");
-                    break;
-                case '\"':
-                    finalValue.append("\\\"");
-                    break;
-                case '\000':
-                    finalValue.append("\\000");
-                    break;
-                default:
-                    if (extraEscapes.length > 0) {
-                        for (char extraEscape : extraEscapes) {
-                            if (c==extraEscape) {
-                                finalValue.append("\\\\");
-                                break;
-                            }
-                        }
-                    }
-                    finalValue.append(c);
+                }
             }
+            finalValue.append(c);
         }
         return finalValue.toString();
     }
@@ -62,56 +23,27 @@ class KonfigerUtil {
         StringBuilder finalValue = new StringBuilder();
         for (int i = 0; i < value.length(); ++i) {
             char c = value.charAt(i);
-            if (c=='\\') {
+            if (c=='/') {
                 if (i==value.length() - 1) {
                     finalValue.append(c);
                     break;
                 }
                 int d = ++i;
-                switch (value.charAt(d)) {
-                    case 't':
-                        finalValue.append("\t");
-                        break;
-                    case 'n':
-                        finalValue.append("\n");
-                        break;
-                    case 'r':
-                        finalValue.append("\r");
-                        break;
-                    case 'f':
-                        finalValue.append("\f");
-                        break;
-                    case 'b':
-                        finalValue.append("\b");
-                        break;
-                    case '\\':
-                        finalValue.append("\\");
-                        break;
-                    case '\'':
-                        finalValue.append("'");
-                        break;
-                    case '"':
-                        finalValue.append("\"");
-                        break;
-                    case '\000':
-                        finalValue.append("\000");
-                        break;
-                    default:
-                        if (extraEscapes.length > 0) {
-                            boolean continua = false;
-                            for (char extraEscape : extraEscapes) {
-                                if (value.charAt(d) == extraEscape) {
-                                    finalValue.append(extraEscape);
-                                    continua = true;
-                                    break;
-                                }
-                            }
-                            if (continua) {
-                                continue;
-                            }
+                if (extraEscapes.length > 0) {
+                    boolean continua = false;
+                    for (char extra : extraEscapes) {
+                        if (value.charAt(d) == extra) {
+                            finalValue.append(extra);
+                            continua = true;
+                            break;
                         }
-                        finalValue.append(value.charAt(d)).append(c);
+                    }
+                    if (continua) {
+                        continue;
+                    }
                 }
+                finalValue.append('/');
+                finalValue.append(value.charAt(d));
                 continue;
             }
             finalValue.append(c);
